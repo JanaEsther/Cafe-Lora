e8e63da31f6bfd304c5e66cf40aca8388fddba88
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-06-29 18:45:18 +0200

    component_Layer

--- src/components/Drink/drink.jsx
+++ src/components/Drink/drink.jsx
@@ -1,3 +1,4 @@
+import { Layer } from '../Layer/layer';
 import './drink.css';
 
 export const Drink = ({ id, name, ordered, image, layers }) => {
@@ -9,6 +10,7 @@ export const Drink = ({ id, name, ordered, image, layers }) => {
         </div>
         <div className="drink__info">
           <h3>{name}</h3>
+          <Layer />
         </div>
       </div>
       <form className="drink__controls">
--- src/components/Layer/layer.jsx
+++ src/components/Layer/layer.jsx
@@ -1,2 +1,13 @@
 import './layer.css'
 
+export const Layer = ({}) =>{
+  return(
+  <div className="layer">
+    <div className="layer__color">
+    <div className="layer__label">
+
+    </div>
+    </div>
+  </div>
+  );
+};
