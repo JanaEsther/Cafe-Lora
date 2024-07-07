8d3e28bcd65dfbffde5ae55afa9985684aadba78
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-07-07 15:58:02 +0200

    detail_order

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <p className="footer__text">© 2024 Café Lora s.r.o. </p>
+        <p className="footer__text">© 2024 Café Lóra s.r.o. </p>
         <div className="footer__content">
           Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2.
         </div>
--- src/components/OrderItem/orderItem.jsx
+++ src/components/OrderItem/orderItem.jsx
@@ -1,9 +1,13 @@
-
- export const OrderItem = ({name, image}) => {
+export const OrderItem = ({ name, image }) => {
   return (
     <div className="order-item">
-    <img src={`http://localhost:4000${image}`} alt={name} className="order-item__image" />
-    <div className="order-item__name"></div>
+      <img
+        src={`http://localhost:4000${image}`}
+        alt={name}
+        className="order-item__image"
+      />
+      <div className="order-item__name"></div>
+      <h3>{name}</h3>
     </div>
-  )
-}
+  );
+};
